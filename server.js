253220ea722ce6ef14f89e269c0d0045049896a1
Jens Alenius <jens.alenius@slu.se>
2022-04-21 11:13:41 +0200

    cors4

--- server.js
+++ server.js
@@ -4,14 +4,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const cors = require('cors');
 
-server.use(
-    cors({
-        origin: true,
-        credentials: true,
-        preflightContinue: false,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    })
-);
+server.use(cors());
 server.options('*', cors());
 server.use(middlewares);
 server.use(router);
