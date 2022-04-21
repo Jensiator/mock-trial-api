710ca6e1fd15a3e5f545ff963cc08e7cdb86eac9
Jens Alenius <jens.alenius@slu.se>
2022-04-21 11:34:30 +0200

    removed it

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const cors = require('cors');
-server.use(cors());
+
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
