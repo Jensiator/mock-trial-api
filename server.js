dec5f79d561a68e4d1470e318a8a1190004beb45
Jens Alenius <jens.alenius@slu.se>
2022-04-25 21:53:37 +0200

    delete

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const fs = require('fs');
+const db = JSON.parse(fs.readFileSync("db.json", "utf-8"));
 
+const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-server.use(middlewares);
+server.use(jsonServer.defaults());
+server.use(jsonServer.bodyParser);
+
+server.post('/trials/:trialId/animals-delete', (req, res) => {
+    const animals = router.db.get("animals").valueOf();
+    for (const id of req.body.ids) {
+        const animal = animals.find(a=>a.id === id);
+        animal.trialId=null;
+    }
+    res.end('200');
+});
+
 server.use(router);
 server.use(jsonServer.rewriter('routes.json'));
 server.listen(process.env.PORT || 3000);
